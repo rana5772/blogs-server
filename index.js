9956da02c2a963169bd1e02da98a3867f4d5f1d1
rana5772 <vaibhav.rana5772@gmail.com>
2026-06-18 21:56:45 +0530

    monodb connection error solved

--- index.js
+++ index.js
@@ -2,28 +2,27 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const colors = require("colors");
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
 // Database Connection
 mongoose
   .connect(process.env.MONGO_URI)
-  .then(() => console.log("DB connected successfully"))
-  .catch((err) => console.error("DB connection error:", err));
+  .then(() => console.log("DB connected successfully".green))
+  .catch((err) => console.error("DB connection error:".red, err));
 
 // Blog Controller
 const blogController = require("./controllers/blogController");
 
 // Blog Routes
 app.get("/api/blogs/get-all", blogController.getAllBlogs);
-// app.post("/api/blogs/new", blogController.createBlog);
 app.get("/api/blogs/:id", blogController.getBlogById);
-// app.put("/api/blogs/:id", blogController.updateBlog);
-// app.delete("/api/blogs/:id", blogController.deleteBlog);
 
-// Root Route for basic testing
+// Root Route
 app.get("/", (req, res) => {
   res.send("server is running");
 });
@@ -32,8 +31,9 @@ app.use("/", (req, res) => {
   res.status(404).send("404 Not Found");
 });
 
-// Start the Server
+// Start Server
 const PORT = process.env.PORT || 8000;
+
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`.cyan);
 });
