af62e90acae7224373db6048171ebff8b130e6a8
rana5772 <vaibhav.rana5772@gmail.com>
2026-06-22 12:11:10 +0530

    title

--- services/groqBlogGenerator.js
+++ services/groqBlogGenerator.js
@@ -34,7 +34,6 @@ Category: ${category}
 Requirements:
 - Generate your own unique topic
 - Around 1000 words
-- Title should be 50-100 characters
 - Markdown format
 - Do NOT include a main title in the article body
 - Do NOT include a heading called "Introduction"
